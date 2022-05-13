b2c6fa65c5ced5fc39fa498827c7b22962dd2998
aymen-ghemam <gh.aymen39@gmail.com>
2022-05-13 11:49:46 +0100

    add slide effect for stations list

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MapView from '../components/MapView'
 import StationView from '../components/StationView'
+import StationsList from '../components/StationsList'
 
-const Map = () => {
+const Map = (page) => {
   return (
     <div className='map'>
         <MapView />
         <StationView/>
+        <StationsList />
     </div>
   )
 }
